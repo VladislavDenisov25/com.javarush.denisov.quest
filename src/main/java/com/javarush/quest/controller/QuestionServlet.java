1caae4b3de7e6fc36b15ee7d3243a5a48b30a634
VladislavDenisov25 <vladislav-denisov@mail.ru>
2026-03-27 19:51:44 +0300

    рефакторинг метода doPost(), вынос логики получения данных в jstl

--- src/main/java/com/javarush/quest/controller/QuestionServlet.java
+++ src/main/java/com/javarush/quest/controller/QuestionServlet.java
@@ -1,18 +1,10 @@
 package com.javarush.quest.controller;
 
-
-import com.javarush.quest.entity.Answers;
-
 import com.javarush.quest.entity.Question;
 import com.javarush.quest.service.QuestionService;
-import jakarta.servlet.RequestDispatcher;
-
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-
+import jakarta.servlet.http.*;
 import java.io.IOException;
 
 
@@ -25,30 +17,17 @@ public class QuestionServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        RequestDispatcher dispatcher = null;
+
         Long idQuestion = Long.parseLong(req.getParameter("idQuestion"));
-
         Question questionCurrent = questionService.getQuestion(idQuestion);
-        String questionCurrentText = questionCurrent.getText();
+
+        req.setAttribute("questionCurrent", questionCurrent);
+
         if (!questionCurrent.getIsFinish()) {
-            Answers answerOne = questionCurrent.getAnswers()[0];
-            Answers answerTwo = questionCurrent.getAnswers()[1];
-            Long answerOneIdNextQuestion = answerOne.getIdNextQuestion();
-            Long answerTwoIdNextQuestion = answerTwo.getIdNextQuestion();
-            String answerOneText = answerOne.getText();
-            String answerTwoText = answerTwo.getText();
-
-            req.setAttribute("answerOneText", answerOneText);
-            req.setAttribute("answerTwoText", answerTwoText);
-            req.setAttribute("answerOneIdNextQuestion", answerOneIdNextQuestion);
-            req.setAttribute("answerTwoIdNextQuestion", answerTwoIdNextQuestion);
-            dispatcher = req.getRequestDispatcher("question.jsp");
+            req.getRequestDispatcher("question.jsp").forward(req, resp);
         } else {
-            dispatcher = req.getRequestDispatcher("finish.jsp");
+            req.getRequestDispatcher("finish.jsp").forward(req, resp);
         }
-        req.setAttribute("questionText", questionCurrentText);
-
-        dispatcher.forward(req, resp);
     }
 
 }
